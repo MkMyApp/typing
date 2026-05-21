4fb0a15c5ce37bdb8d183787f09df231bd61afef
MkMyApp <mshinoda@big.or.jp>
2026-05-21 18:38:54 +0900

    Add files via upload

--- custom/custom.js
+++ custom/custom.js
@@ -1,4 +1,4 @@
-  let TITLET_MSG
+  let TITLE_MSG
   let START_MSG
   let INPUT_MSG
   let RANDOM
@@ -13,7 +13,7 @@
   }
 
   function updateSettings() {
-	  document.title = document.getElementById("TITLET_MSG").value;
+	  document.title = document.getElementById("TITLE_MSG").value;
 	  START_MSG = document.getElementById("START_MSG").value;
 	  INPUT_MSG = document.getElementById("INPUT_MSG").value;
 	  RANDOM    = Number(document.getElementById("RANDOM").value);
@@ -98,4 +98,38 @@ document.addEventListener("DOMContentLoaded", () => {
     maxLenButton.addEventListener("click", setMaxLength);
   }
 });
+
+function composeText() {
+
+// 1. 各項目の値を取得
+const titleMsg = document.getElementById('TITLE_MSG').value;
+const startMsg = document.getElementById('START_MSG').value;
+const inputMsg = document.getElementById('INPUT_MSG').value;
+const randomVal = document.getElementById('RANDOM').value;
+const widthVal = document.getElementById('WIDTH').value;
+
+// 2. テキストエリアの要素を取得
+const textArea = document.getElementById('txtdata');
+const textComp = document.getElementById('txtComp');
+
+// 3. 追加したい「const部分」を組み立てる
+const configText = `<script>
+const TITLE_MSG = "${titleMsg}";
+const START_MSG = "${startMsg}";
+const INPUT_MSG = "${inputMsg}";
+const RANDOM = "${randomVal}";
+const WIDTH = "${widthVal}";
+<\/script>\n`;
+
+const scaleText = `<!-------10--------20--------30--------40--------->\n`
+const headText = `<textarea id="txtdata" hidden>\n`;
+
+const footText = `</textarea>\n`;
+
+const textVal = textArea.value;
+
+// テキストエリアに合成結果を出力
+textComp.value = configText + scaleText + headText + textVal + footText + scaleText;
+textComp.style.display = "block";
+}
 
