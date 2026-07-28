49f06a9e028c533eaed863ce969b33d6b6ea6e38
MkMyApp <mshinoda@big.or.jp>
2026-07-29 01:25:45 +0900

    修正

--- labs/download/typing.js
+++ labs/download/typing.js
@@ -282,3 +282,14 @@ editorEl.addEventListener('keydown', e => {
 
 init()
 
+const expEl = document.getElementById('exp');
+if (window.self !== window.top) {
+  if (expEl) expEl.hidden = true;
+}
+
+if (expEl) {
+  expEl.addEventListener('click', function() {
+    this.hidden = true;
+    editorEl.focus();
+  });
+}
