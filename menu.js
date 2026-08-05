2dcfcd7fab007a8712041d6b8bdc0c4fb6cdf2eb
MkMyApp <mshinoda@big.or.jp>
2026-08-05 18:25:43 +0900

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,3 +1,12 @@
+const iframeElement = document.getElementById('frm_type');
+
+iframeElement.addEventListener('load', function() {
+//iframeの読み込み完了時に高さを合わせる処理
+    // iframe内部のコンテンツの高さを取得して適用
+    const contentHeight = iframeElement.contentWindow.document.documentElement.scrollHeight;
+    iframeElement.style.height = contentHeight + 'px';
+});
+
 // ランダムでsrcを設定
 const menuList = [
 'kanji/部活動.html',
