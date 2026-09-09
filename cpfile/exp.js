581efda24badb45af93351fe871c01eb521b3119
MkMyApp <mshinoda@big.or.jp>
2026-09-09 09:34:40 +0900

    update

--- cpfile/exp.js
+++ cpfile/exp.js
@@ -1,9 +1,19 @@
-window.addEventListener('keydown', (event) => {
-  if (event.key === 'Escape') {
-    if (document.getElementById('exp').style.display === "none") {
-      document.getElementById('exp').style.display = "block";
+<script>
+const exp = document.getElementById('exp');
+if (exp) { exp.textContent = exp.textContent.replace(/^\n/, ''); }
+expNone();
+
+function expNone() {
+  if (exp) {
+    if (exp.style.display === "none") {
+      exp.style.display = "block";
     } else {
-      document.getElementById('exp').style.display = "none";
+      exp.style.display = "none";
     }
   }
+}
+
+window.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape') { expNone(); } // ← `expNome` を `expNone` に修正
 });
+</script>
